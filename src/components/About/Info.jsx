10a0e31d1ce6d724997aa3a26f1f044cb8da8daf
Gautampatel-28 <gautampatel7233@gmail.com>
2024-09-19 15:34:25 +0530

    service pages

--- src/components/About/Info.jsx
+++ src/components/About/Info.jsx
@@ -5,17 +5,17 @@ const Info = () => {
     <>
         <div className='about_info grid'>
             <div className='about_box'>
-            <i class='bx bx-award about_icon'></i>
+            <i className='bx bx-award about_icon'></i>
                 <h3 className='about_title'>Fresher</h3>
                 <span className='about_subtitle'>6 months Intern</span>
             </div>
             <div className='about_box'>
-            <i class='bx bx-briefcase-alt about_icon'></i>
+            <i className='bx bx-briefcase-alt about_icon'></i>
                 <h3 className='about_title'>Completed</h3>
                 <span className='about_subtitle'>5+</span>
             </div>
             <div className='about_box'>
-            <i class='bx bx-support about_icon'></i>
+            <i className='bx bx-support about_icon'></i>
                 <h3 className='about_title'>Support</h3>
                 <span className='about_subtitle'>Online 24/7</span>
             </div>
--- src/components/Skills/Backend.jsx
+++ src/components/Skills/Backend.jsx
@@ -11,28 +11,28 @@ const Backend = () => {
                     <div className='skills_data'>
                         <i className='bx bx-badge-check'></i>
 
-                        <di>
+                        <div>
                             <h3 className='skills_name'>Php</h3>
                             <span className='skills_level'>Basic</span>
-                        </di>
+                        </div>
                     </div>
 
                     <div className='skills_data'>
                         <i className='bx bx-badge-check'></i>
 
-                        <di>
+                        <div>
                             <h3 className='skills_name'>Node.js</h3>
                             <span className='skills_level'>Basic</span>
-                        </di>
+                        </div>
                     </div>
 
                     <div className='skills_data'>
                         <i className='bx bx-badge-check'></i>
 
-                        <di>
+                        <div>
                             <h3 className='skills_name'>FireBase</h3>
                             <span className='skills_level'>Intermediate</span>
-                        </di>
+                        </div>
                     </div>
                 </div>
 
@@ -40,28 +40,28 @@ const Backend = () => {
                     <div className='skills_data'>
                         <i className='bx bx-badge-check'></i>
 
-                        <di>
+                        <div>
                             <h3 className='skills_name'>MongoDB</h3>
                             <span className='skills_level'>Intermediate</span>
-                        </di>
+                        </div>
                     </div>
 
                     <div className='skills_data'>
                         <i className='bx bx-badge-check'></i>
 
-                        <di>
+                        <div>
                             <h3 className='skills_name'>Express.js</h3>
                             <span className='skills_level'>Intermediate</span>
-                        </di>
+                        </div>
                     </div>
 
                     <div className='skills_data'>
                         <i className='bx bx-badge-check'></i>
 
-                        <di>
+                        <div>
                             <h3 className='skills_name'>MySql</h3>
                             <span className='skills_level'>Intermediate</span>
-                        </di>
+                        </div>
                     </div>
                 </div>
             </div>
